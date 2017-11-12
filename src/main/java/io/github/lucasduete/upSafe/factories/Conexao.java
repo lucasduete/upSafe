fa014e4018177cf640ff79d4dc3c4d7b74f348f7
Lucas Duete <lm.duete@gmail.com>
2017-11-11 23:21:27 -0300

    Modificando Configurações de Conexão.

--- src/main/java/io/github/lucasduete/upSafe/factories/Conexao.java
+++ src/main/java/io/github/lucasduete/upSafe/factories/Conexao.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-    private static final String url = "jdbc:postgresql://ec2-54-163-233-201.compute-1.amazonaws.com:5432/dc556k0psngs08";
-    private static final String usuario = "lxujnjxkxuivbs";
-    private static final String senha = "3f6ea33fb42d35767312fccacc67d211070a7dbcb0b9a698bd0885e8cedbdd2f";
+    private static final String url = "jdbc:postgresql://ec2-107-22-211-182.compute-1.amazonaws.com/d5qptji4icd9l0";
+    private static final String usuario = "uigpviycyhaytu";
+    private static final String senha = "ed4f5570118b0c9cbae684319e65979ed1d9483747b93952cc5bffed0f275da5";
     
     public static Connection getConnection() throws SQLException, ClassNotFoundException{
         Class.forName("org.postgresql.Driver");
