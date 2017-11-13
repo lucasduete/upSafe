b4b0dc842f358d8b6213bd95648004a00c1a04b1
Lucas Duete <lm.duete@gmail.com>
2017-11-13 20:23:39 -0300

    Refatorando Classe Que Converte Para Base64 para Funcionar com Qualquer Arquivo.

---
+++ src/main/java/io/github/lucasduete/upSafe/resources/FileManagement.java
@@ -0,0 +1,32 @@
+package io.github.lucasduete.upSafe.resources;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+public class FileManagement {
+
+    private static final String PATHNAME = "";
+
+    public static String encodeFile(File file) throws IOException {
+
+        byte[] bytes = new byte[(int)file.length()];
+        new FileInputStream(file).read(bytes);
+
+        String fileBase64 = new String(Base64.getEncoder().encodeToString(bytes));
+        return fileBase64;
+    }
+
+    public static File decodeFile(String fileBase64) throws IOException {
+
+        byte[] fileBytes = Base64.getDecoder().decode(fileBase64);
+
+        FileOutputStream fileOutputStream = new FileOutputStream(PATHNAME + fileBase64.hashCode());
+        fileOutputStream.write(fileBytes);
+
+        File file = new File(PATHNAME + fileBase64.hashCode());
+        return file;
+    }
+}
