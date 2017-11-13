e2370211ae9dc2d352a903fc4b6ca547f359d893
Lucas Duete <lm.duete@gmail.com>
2017-11-13 20:25:18 -0300

    Refatorando Método de Download de Arquivos para Restringir Tipos de Arquivos que podem ser Baixados, Refatorando Método de Upload de Arquivos para Restringir Tipos de Arquivos que podem ser Enviados.

--- src/main/java/io/github/lucasduete/upSafe/controllers/FileController.java
+++ src/main/java/io/github/lucasduete/upSafe/controllers/FileController.java
@@ -4,12 +4,14 @@ import io.github.lucasduete.upSafe.dao.ArquivoDao;
 import io.github.lucasduete.upSafe.infraSecurity.FilterDetect;
 import io.github.lucasduete.upSafe.infraSecurity.Security;
 import io.github.lucasduete.upSafe.models.Arquivo;
+import io.github.lucasduete.upSafe.resources.FileManagement;
 
 import javax.ws.rs.*;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -29,6 +31,10 @@ public class FileController {
 
         ArquivoDao arquivoDao = new ArquivoDao();
 
+        if (!(file.getNome().contains("png") || file.getNome().contains("jpeg") || file.getNome().contains("jpg")
+                || file.getNome().contains("pdf") || file.getNome().contains("mp4") || file.getNome().contains("mp3")))
+            return Response.status(415).build();
+
         try {
             arquivoDao.salvar(file);
 
@@ -45,7 +51,7 @@ public class FileController {
 
     @GET
     @Security
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces({ "image/jpg", "image/png", MediaType.APPLICATION_OCTET_STREAM, "application/pdf", "audio/mp3" })
     @Path("downloadArquivo/{idArquivo}")
     public Response download(@PathParam("idArquivo") int idArquivo,
                              @Context ContainerRequestContext requestContext) {
@@ -61,11 +67,28 @@ public class FileController {
             if(file.getIdUsuario() != Integer.parseInt(FilterDetect.getToken(requestContext)))
                 return Response.status(Response.Status.UNAUTHORIZED).build();
 
-            return Response.ok(file).build();
+
+            if(file.getNome().contains("mp3"))
+                return Response.ok(FileManagement.decodeFile(file.getContent()), "audio/mp3").build();
+
+            if(file.getNome().contains("pdf"))
+                return Response.ok(FileManagement.decodeFile(file.getContent()), "application/pdf").build();
+
+            if(file.getNome().contains("jpg"))
+                return Response.ok(FileManagement.decodeFile(file.getContent()), "image/jpg").build();
+
+            if(file.getNome().contains("png"))
+                return Response.ok(FileManagement.decodeFile(file.getContent()), "image/png").build();
+
+            if(file.getNome().contains("mp4"))
+                return Response.ok(FileManagement.decodeFile(file.getContent()), MediaType.APPLICATION_OCTET_STREAM).build();
+
+            return Response.status(415).build();
+
         } catch (SQLException | NullPointerException e) {
             e.printStackTrace();
             return Response.status(Response.Status.BAD_REQUEST).build();
-        } catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException | IOException e) {
             e.printStackTrace();
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
